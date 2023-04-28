c377619c62fb9fccb8585a35c1215adb0bcfb1a3
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-29 00:01:20 +0100

    Getting data from csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import turtle 
+import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,6 +7,22 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+gueesed_states = []
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+while len(gueesed_states) < 50:
+  answer_state = screen.textinput(title=f"{len(gueesed_states)}/50 States Correct", prompt="What's another state name?").title()
+  print(answer_state)
+
+  if answer_state in all_states:
+      gueesed_states.append(answer_state)
+      t = turtle.Turtle()
+      t.hideturtle()
+      t.penup()
+      state_data = data[data.state == answer_state]
+      t.goto(int(state_data.x), int(state_data.y))
+      t.write(answer_state)
+
+
+screen.exitonclick()
