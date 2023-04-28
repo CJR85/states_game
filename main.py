6cb26b947fbdc20369edcf3f937373fa737f7569
Calvin Roberts <roberts.calvin85@gmail.com>
2023-04-29 00:14:24 +0100

    Saving data to .csv

--- main.py
+++ main.py
@@ -9,14 +9,23 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
-gueesed_states = []
+guessed_states = []
 
-while len(gueesed_states) < 50:
-  answer_state = screen.textinput(title=f"{len(gueesed_states)}/50 States Correct", prompt="What's another state name?").title()
+while len(guessed_states) < 50:
+  answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state name?").title()
   print(answer_state)
 
+  if answer_state == "Exit":
+     missing_states = []
+     for state in all_states:
+        if state not in guessed_states:
+           missing_states.append(state)
+     new_data = pandas.DataFrame(missing_states)
+     new_data.to_csv("states_to_learn.csv") # States to learn.csv
+     
+     break
   if answer_state in all_states:
-      gueesed_states.append(answer_state)
+      guessed_states.append(answer_state)
       t = turtle.Turtle()
       t.hideturtle()
       t.penup()
@@ -25,4 +34,3 @@ while len(gueesed_states) < 50:
       t.write(answer_state)
 
 
-screen.exitonclick()
